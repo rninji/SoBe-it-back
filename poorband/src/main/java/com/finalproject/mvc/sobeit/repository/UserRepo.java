b3fc290e97430a301d36781140c52a34df101c10
parfait2 <parkyl0208@naver.com>
2023-05-15 16:06:45 +0900

    Fix findByUser_id

--- poorband/src/main/java/com/finalproject/mvc/sobeit/repository/UserRepo.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/repository/UserRepo.java
@@ -6,12 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<Users, Long> {
-<<<<<<< HEAD
 
-    Users findByUserId(String user_id);
-=======
     Users findByUser_id(String user_id);
->>>>>>> ee8ee274aa21e7185dae385fc49f7d35d5a6db53
     Users findByEmail(String email);
     Boolean existsByUser_id(String user_id);
     Boolean existsByEmail(String email);
