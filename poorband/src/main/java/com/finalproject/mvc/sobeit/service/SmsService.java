6a56bbf4d17c00a7b2e3bffde10dbdc3ef48e5ff
1wooo <dnjsdn2468@naver.com>
2023-05-15 16:16:02 +0900

    [feat] : SmsService.java makeSignature 메소드 추가

---
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/service/SmsService.java
@@ -0,0 +1,56 @@
+package com.finalproject.mvc.sobeit.service;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+@RequiredArgsConstructor
+public class SmsService {
+    @Value("${naver-cloud-sms.accessKey")
+    private String accessKey;
+    @Value("${naver-cloud-sms.secretKey")
+    private String secretKey;
+    @Value("${naver-cloud-sms.accessKey")
+    private String serviceId;
+    @Value("${naver-cloud-sms.accessKey")
+    private String phone;
+
+    public String makeSignature(Long time) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
+        String space = " ";
+        String newLine = "\n";
+        String method = "POST";
+        String url = "/sms/v2/services/"+ this.serviceId+"/messages";
+        String timestamp = time.toString();
+        String accessKey = this.accessKey;
+        String secretKey = this.secretKey;
+
+        String message = new StringBuilder()
+                .append(method)
+                .append(space)
+                .append(url)
+                .append(newLine)
+                .append(timestamp)
+                .append(newLine)
+                .append(accessKey)
+                .toString();
+
+        SecretKeySpec signingKey = new SecretKeySpec(secretKey.getBytes("UTF-8"), "HmacSHA256");
+
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(signingKey);
+
+        byte[] rawHmac = mac.doFinal(message.getBytes("UTF-8"));
+        String encodeBase64String = Base64.encodeBase64String(rawHmac);
+
+        return encodeBase64String;
+    }
+
+}
