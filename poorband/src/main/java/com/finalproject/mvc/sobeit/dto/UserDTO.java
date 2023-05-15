fc25fa7c734ae77d2fdc571de34b57701dd21771
C0012S <alwaysh1002@gmail.com>
2023-05-15 17:35:26 +0900

    [Add] UserDTO

---
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.finalproject.mvc.sobeit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long user_seq;
+    private String token;
+    private String user_id;
+    private String email;
+    private String introduction;
+    private String user_name;
+    private String password;
+    private String nickname;
+    private String user_tier;
+    private Long challenge_count;
+    private String phone_number;
+    private String profile_image_url;
+}
