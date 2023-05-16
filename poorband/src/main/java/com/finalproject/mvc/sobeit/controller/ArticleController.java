9ada9f94b599c8b92d0672b34e68d3e471c0790b
rninji <yellowminji@naver.com>
2023-05-16 15:29:27 +0900

    [MOD] ArticleController Mapping 방법 수정

--- poorband/src/main/java/com/finalproject/mvc/sobeit/controller/ArticleController.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/controller/ArticleController.java
@@ -3,12 +3,15 @@ package com.finalproject.mvc.sobeit.controller;
 import com.finalproject.mvc.sobeit.entity.Article;
 import com.finalproject.mvc.sobeit.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/article")
 public class ArticleController {
     @Autowired
     ArticleService articleService;
@@ -18,7 +21,7 @@ public class ArticleController {
      * @param article
      * @return
      */
-    @RequestMapping("/article/write")
+    @PostMapping("/write")
     public String writeArticle(Article article){
         articleService.writeArticle(article);
         return("write");
@@ -29,7 +32,7 @@ public class ArticleController {
      * @param article
      * @return
      */
-    @RequestMapping("article/update")
+    @PostMapping("/update")
     public String updateArticle(Article article){
         articleService.updateArticle(article);
         return("update");
@@ -40,7 +43,7 @@ public class ArticleController {
      * @param articleSeq
      * @return
      */
-    @RequestMapping("/article/delete")
+    @PostMapping("/delete")
     public String deleteArticle(Long articleSeq){
         articleService.deleteArticle(articleSeq);
         return("delete");
@@ -51,7 +54,7 @@ public class ArticleController {
      * @param articleSeq
      * @return
      */
-    @RequestMapping("/article/detail")
+    @GetMapping("/detail")
     public Article selectArticleById(Long articleSeq){
         Article article = articleService.selectArticleById(articleSeq);
         if (article == null){
@@ -65,7 +68,7 @@ public class ArticleController {
      * 글 전체 조회
      * @return
      */
-    @RequestMapping("/article/selectAll")
+    @GetMapping("/selectAll")
     public List<Article> selectArticleAll(){
         List<Article> list = articleService.selectAllArticle();
         return list;
