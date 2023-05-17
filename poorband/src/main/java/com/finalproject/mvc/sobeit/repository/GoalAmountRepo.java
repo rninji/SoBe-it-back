e64d30c8fbd1a3b0979265bcebd134535211baa2
1wooo <dnjsdn2468@naver.com>
2023-05-17 11:11:00 +0900

    [Fix] : 외래키 변수명 변경

--- poorband/src/main/java/com/finalproject/mvc/sobeit/repository/GoalAmountRepo.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/repository/GoalAmountRepo.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface GoalAmountRepo extends JpaRepository<GoalAmount, Long> {
-    @Query("select a from GoalAmount a where a.userSeq = ?1 order by a.startDate desc")
+    @Query("select a from GoalAmount a where a.user = ?1 order by a.startDate desc")
     List<GoalAmount> findGoalAmountByUserId(String userId);
 }
