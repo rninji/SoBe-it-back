6c01960278bbfa9b53e9449706239f2767bcebe0
rninji <yellowminji@naver.com>
2023-05-17 15:50:13 +0900

    [ADD] ArticleDTO, ArticleResponseDTO 추가

---
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/dto/ArticleDTO.java
@@ -0,0 +1,23 @@
+package com.finalproject.mvc.sobeit.dto;
+
+import com.finalproject.mvc.sobeit.entity.Users;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleDTO {
+    private Users user;
+    private int status;
+    private String imageUrl;
+    private String expenditureCategory;
+    private Long amount;
+    private String financialText;
+    private String articleText;
+    private String articleType;
+    private LocalDate consumptionDate;
+    private String isAllowed;
+}
