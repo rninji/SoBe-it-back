2f14c31f3fa4f6af46c1e82a22de16c0896398c3
parfait2 <parkyl0208@naver.com>
2023-05-18 16:36:04 +0900

    [Feat] FollowingRepo - 팔로워, 팔로잉 목록 가져오기

--- poorband/src/main/java/com/finalproject/mvc/sobeit/repository/FollowingRepo.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/repository/FollowingRepo.java
@@ -14,14 +14,17 @@ import java.util.Optional;
 @Repository
 public interface FollowingRepo extends JpaRepository<Following, Long>, QuerydslPredicateExecutor<Following> {
 
-    @Query(value = "select count(f) from Following f join Users u on f.user = ?1")
-    int followingCnt(Users user);
+    @Query(value = "select count(f) from Following f join Users u on f.user.userSeq = ?1")
+    int followingCnt(Long userSeq);
 
     @Query(value = "select count(f) from Following f join Users u on f.followingUserSeq = ?1")
     int followerCnt(Long userSeq);
 
-    @Query("select f from Following f where f.user = ?1")
-    List<Following> findArticleThatUserFollows(Users user);
+    @Query("select u from Users u join Following f where f.user.userSeq = ?1")
+    List<Users> findArticleThatUserFollows(Long userSeq);
+
+    @Query("select u from Users u join Following f where f.followingUserSeq = ?1")
+    List<Users> findArticleThatUserFollowed(Long userSeq);
 
     @Query("select f from Following f where f.user = ?1 and f.followingUserSeq=?2")
     Optional<Following> findByFollowingAndFollower(Users followee, Users followingUser_seq);
