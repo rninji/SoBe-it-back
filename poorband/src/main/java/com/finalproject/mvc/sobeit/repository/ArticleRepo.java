e9dcd8107735196e26a8907784dec9f35ad61d92
rninji <yellowminji@naver.com>
2023-05-18 11:40:24 +0900

    [MOD] requestBody로 투표하기 변경

--- poorband/src/main/java/com/finalproject/mvc/sobeit/repository/ArticleRepo.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/repository/ArticleRepo.java
@@ -11,13 +11,13 @@ import java.util.List;
 
 @Repository
 public interface ArticleRepo extends JpaRepository<Article, Long> {
-    @Query("select a from Article a where a.user = ?1 order by a.writtenDate desc")
-    List<Article> findArticlesByUser(String user_id);
+    //@Query("select a from Article a where a.user = ?1 order by a.writtenDate desc")
+    //List<Article> findArticlesByUser(String user_id);
 
     @Query("select a from Article a where a.articleText like %?1%")
     List<Article> findArticlesByArticleText(String articleText);
 
-    Article findByUserId(String userId);
+    //Article findByUserId(String userId);
 
 
     @Query("SELECT u, a FROM Users u LEFT JOIN Article a ON a.user =:user")
