4d4e5e5dd6c7b2b0dc77a2af2b9ede7d1c076303
parfait2 <parkyl0208@naver.com>
2023-05-18 15:57:33 +0900

    [Comment] ProfileController

--- poorband/src/main/java/com/finalproject/mvc/sobeit/controller/ProfileController.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/controller/ProfileController.java
@@ -2,28 +2,16 @@ package com.finalproject.mvc.sobeit.controller;
 
 import com.finalproject.mvc.sobeit.dto.*;
 import com.finalproject.mvc.sobeit.entity.Article;
-import com.finalproject.mvc.sobeit.entity.Following;
 import com.finalproject.mvc.sobeit.entity.GoalAmount;
 import com.finalproject.mvc.sobeit.entity.Users;
-import com.finalproject.mvc.sobeit.responses.ApiResponse;
 import com.finalproject.mvc.sobeit.service.ProfileService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.parameters.P;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -35,9 +23,12 @@ public class ProfileController {
 
     /**
      * 프로필 유저 정보 가져오기
+     * : 로그인된 사용자, 다른 사용자 프로필 조회
+     * @param user
+     * @return 프로필에 표시되는 사용자 정보
      * */
     @RequestMapping("/profileinfo")
-    public ProfileUserDTO profileinfo(@AuthenticationPrincipal Users user) {
+    public ProfileUserDTO profileinfo(Users user) {
         ProfileUserDTO profileUserDTO = profileService.selectUserInfo(user);
 
         return profileUserDTO;
@@ -45,9 +36,13 @@ public class ProfileController {
 
     /**
      * 작성한 글 목록 가져오기
+     * : 로그인된 사용자 또는 다른 사용자가 작성한 글 목록 가져오기.
+     *   추후 공개 여부에 따라 param을 @Authentication Users user, Users targetUser로 변경
+     * @param user
+     * @return 작성한 글 목록
      * */
     @RequestMapping("/myarticle")
-    public List<Article> articleList(@AuthenticationPrincipal Users user) {
+    public List<Article> articleList(Users user) {
         List<Article> list = profileService.selectMyArticle(user);
 
         return list;
@@ -55,9 +50,11 @@ public class ProfileController {
 
     /**
      * 도전 과제 정보 가져오기
+     * @param user
+     * @return 도전 과제 목록
      * */
     @RequestMapping("/challenge")
-    public List<GoalAmount> challenge(@AuthenticationPrincipal Users user) {
+    public List<GoalAmount> challenge(Users user) {
         List<GoalAmount> list = profileService.selectChallenge(user);
 
         return list;
@@ -65,6 +62,9 @@ public class ProfileController {
 
     /**
      * 유저 프로필 편집 저장
+     * @param loggedInUser
+     * @param profile
+     * @return 성공 시 "success", 실패 시 Error message
      * */
     @RequestMapping("/save")
     public ResponseEntity<Object> save(@AuthenticationPrincipal Users loggedInUser, @RequestBody Users profile) {
@@ -91,9 +91,11 @@ public class ProfileController {
 
     /**
      * 팔로잉 정보 가져오기
+     * @param user
+     * @return 사용자의 팔로잉 목록
      * */
     @RequestMapping("/following")
-    public List<Users> following(@AuthenticationPrincipal Users user) {
+    public List<Users> following(Users user) {
         List<Users> list = profileService.selectFollowing(user);
 
         return list;
@@ -101,9 +103,11 @@ public class ProfileController {
 
     /**
      * 팔로워 정보 가져오기
+     * @param user
+     * @return 사용자의 팔로워 목록
      * */
     @RequestMapping("/follower")
-    public List<Users> follower(@AuthenticationPrincipal Users user) {
+    public List<Users> follower(Users user) {
         List<Users> list = profileService.selectFollower(user);
 
         return list;
@@ -111,6 +115,9 @@ public class ProfileController {
 
     /**
      * 팔로잉 해제
+     * @param user
+     * @param targetUser
+     * @return 성공 시 "success", 실패 시 Error message
      * */
     @RequestMapping("/deleteFollowing")
     public ResponseEntity<?> deleteFollowing(@AuthenticationPrincipal Users user, Users targetUser) throws Exception {
@@ -128,6 +135,9 @@ public class ProfileController {
 
     /**
      * 팔로우 추가
+     * @param user
+     * @param targetUser
+     * @return 성공 시 "success", 실패 시 Error message
      * */
     @RequestMapping("/addFollow")
     public ResponseEntity<?> addFollow(@AuthenticationPrincipal Users user, Users targetUser) {
