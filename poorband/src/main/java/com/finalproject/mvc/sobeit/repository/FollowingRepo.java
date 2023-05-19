5a78a22be7e48d6d0d9b8350fadc115159d6f387
parfait2 <parkyl0208@naver.com>
2023-05-19 09:58:59 +0900

    [Update] FollowingRepo - Query 수정

--- poorband/src/main/java/com/finalproject/mvc/sobeit/repository/FollowingRepo.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/repository/FollowingRepo.java
@@ -6,6 +6,7 @@ import com.finalproject.mvc.sobeit.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,19 +15,16 @@ import java.util.Optional;
 
 public interface FollowingRepo extends JpaRepository<Following, Long>, QuerydslPredicateExecutor<Following> {
 
-    @Query(value = "select count(f) from Following f join Users u on f.user.userSeq = ?1")
-    int followingCnt(Long userSeq);
+    @Query(value = "select count(f) from Following f where f.user.userSeq = :#{#user.userSeq}")
+    int followingCnt(@Param("user") Users user);
 
-    @Query(value = "select count(f) from Following f join Users u on f.followingUserSeq = ?1")
-    int followerCnt(Long userSeq);
+    @Query(value = "select count(f) from Following f where f.followingUserSeq = :#{#user.userSeq}")
+    int followerCnt(@Param("user") Users user);
 
-    @Query("select u from Users u join Following f where f.user.userSeq = ?1")
-    List<Users> findArticleThatUserFollows(Long userSeq);
+    @Query("select u from Users u join Following f on u.userSeq = f.user.userSeq where f.user.userSeq = :#{#user.userSeq}")
+    List<Users> findArticleThatUserFollows(@Param("user") Users user);
 
-    @Query("select u from Users u join Following f where f.followingUserSeq = ?1")
-    List<Users> findArticleThatUserFollowed(Long userSeq);
-
-    @Query("select f from Following f where f.user.userSeq = ?1 and f.followingUserSeq=?2")
-    Optional<Following> findByFollowingAndFollower(Long userSeq, Long followingUserSeq);
+    @Query("select f from Following f where f.user.userSeq = :#{#targetUser.userSeq} and f.followingUserSeq=:#{#user.userSeq}")
+    Optional<Following> findByFollowingAndFollower(@Param("user") Users user, @Param("targetUser") Users targetUser);
 
 }
