29890159f0054982a86159b490bd61c6e372a8c5
C0012S <alwaysh1002@gmail.com>
2023-05-20 23:39:02 +0900

    [Update] ReplyLikeRepo - 좋아요 개수 확인 쿼리 추가

--- poorband/src/main/java/com/finalproject/mvc/sobeit/repository/ReplyLikeRepo.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/repository/ReplyLikeRepo.java
@@ -4,7 +4,11 @@ import com.finalproject.mvc.sobeit.entity.Reply;
 import com.finalproject.mvc.sobeit.entity.ReplyLike;
 import com.finalproject.mvc.sobeit.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReplyLikeRepo extends JpaRepository<ReplyLike, Long> {
     ReplyLike findByReplyAndUser(Reply reply, Users user);
+
+    @Query("select count(*) from ReplyLike r where r.reply.replySeq = ?1")
+    int findCountReplyLikeByReplySeq(Long replySeq);
 }
