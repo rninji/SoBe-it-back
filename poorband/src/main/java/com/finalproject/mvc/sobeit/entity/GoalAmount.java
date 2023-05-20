c380c16f6a8616a0ce01fabb7bd1351da3e1de48
qkrck <qkrcksdnr71@naver.com>
2023-05-20 18:16:20 +0900

    [Update] 도전과제에서 반복 주기(routine) 추가 [Comment] GoalAmount에 주석 추가

--- poorband/src/main/java/com/finalproject/mvc/sobeit/entity/GoalAmount.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/entity/GoalAmount.java
@@ -15,24 +15,27 @@ import java.time.LocalDateTime;
 public class GoalAmount {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long goalAmountSeq;
+    private Long goalAmountSeq; // 도전과제 고유번호
 
     @Column(nullable = false)
-    private Long goalAmount;
+    private Long goalAmount; // 도전과제 목표금액
 
     @Column(nullable = false)
-    private String title;
+    private String title; // 도전과제 제목
 
     @ManyToOne
     @JoinColumn(name = "userSeq", referencedColumnName = "userSeq", nullable = false)
-    private Users user;
+    private Users user; // 해당하는 도전과제를 도전하는 사용자
 
     @Column(nullable = false)
-    private LocalDateTime startDate;
+    private LocalDateTime startDate; // 도전과제 시작일
 
     @Column(nullable = false)
-    private LocalDateTime endDate;
+    private LocalDateTime endDate; // 도전과제 종료일
 
     @Column(nullable = false, length = 4)
-    private String isSuccess;
+    private String isSuccess; // 도전과제 성공 여부(성공 or 실패)
+
+    @Column(nullable = false)
+    private String routine; // 도전과제 반복 주기(매일 or 전체기간)
 }
