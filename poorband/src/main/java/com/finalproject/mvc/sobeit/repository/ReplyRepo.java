9521843eaa6d28709d7f5054a216d1cb52a0516a
C0012S <alwaysh1002@gmail.com>
2023-05-21 21:42:40 +0900

    [Update] ReplyRepo - 해당 글에 작성된 댓글 찾기 쿼리 추가

--- poorband/src/main/java/com/finalproject/mvc/sobeit/repository/ReplyRepo.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/repository/ReplyRepo.java
@@ -5,6 +5,8 @@ import com.finalproject.mvc.sobeit.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface ReplyRepo extends JpaRepository<Reply, Long> {
     Reply findByReplySeq(Long replySeq);
 
@@ -18,4 +20,8 @@ public interface ReplyRepo extends JpaRepository<Reply, Long> {
     // 댓글을 작성한 사용자 정보 찾기
     @Query("select u from Reply r, Users u where r.user.userSeq = u.userSeq")
     Users findReplyUsersByUserSeq(Long userSeq);
+
+    // 해당 글에 작성된 댓글 찾기
+    @Query("select r from Reply r, Article a where r.article.articleSeq = a.articleSeq")
+    List<Reply> findReplyByArticleSeq(Long articleSeq);
 }
