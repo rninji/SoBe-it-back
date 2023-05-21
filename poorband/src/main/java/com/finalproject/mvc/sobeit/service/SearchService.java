8a36012ddfd57a234bc1ef04d16b5660601ef36b
qkrck <qkrcksdnr71@naver.com>
2023-05-21 19:16:41 +0900

    [Fix] 유저 검색 수정

--- poorband/src/main/java/com/finalproject/mvc/sobeit/service/SearchService.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/service/SearchService.java
@@ -1,7 +1,9 @@
 package com.finalproject.mvc.sobeit.service;
 
 import com.finalproject.mvc.sobeit.dto.ArticleResponseDTO;
+import com.finalproject.mvc.sobeit.dto.ProfileDTO;
 import com.finalproject.mvc.sobeit.entity.Users;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.List;
 
@@ -9,7 +11,7 @@ public interface SearchService {
     /**
      * 사용자(users) 검색
      **/
-    List<Users> usersSearch(String inputText);
+    List<ProfileDTO> usersSearch(@AuthenticationPrincipal Users loggedInUser, String userId, String inputText);
 
     /**
      * 게시글(Articles) 검색
--- poorband/src/main/java/com/finalproject/mvc/sobeit/service/SearchServiceImpl.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/service/SearchServiceImpl.java
@@ -1,10 +1,12 @@
 package com.finalproject.mvc.sobeit.service;
 
 import com.finalproject.mvc.sobeit.dto.ArticleResponseDTO;
+import com.finalproject.mvc.sobeit.dto.ProfileDTO;
 import com.finalproject.mvc.sobeit.entity.Users;
 import com.finalproject.mvc.sobeit.repository.ArticleRepo;
 import com.finalproject.mvc.sobeit.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -18,21 +20,23 @@ public class SearchServiceImpl implements SearchService{
 
     private final UserRepo userRep;
     private final ArticleRepo articleRep;
-    private  final ArticleServiceImpl articleService;
+    private final ArticleServiceImpl articleService;
+    private final ProfileServiceImpl profileService;
 
     /**
      * 사용자(users) 검색
      **/
     @Override
-    public List<Users> usersSearch(String inputText) {
-        List<Users> userSearchResult;
-        try{
-            userSearchResult = userRep.findAllByUserId(inputText);
-            System.out.println("userSearchResult = " + userSearchResult);
-        } catch (NullPointerException exception) { // 검색어가 포함되는 userId가 없을 경우(null)
-            throw new RuntimeException("검색어와 포함된 유저가 없습니다.");
+    public List<ProfileDTO> usersSearch(@AuthenticationPrincipal Users loggedInUser, String userId, String inputText) {
+        List<Long> searchUserSeqList = userRep.findAllByUserId(inputText);
+        List<ProfileDTO> searchUserList = new ArrayList<>();
+        if (searchUserSeqList == null || searchUserSeqList.size() == 0){
+            throw new RuntimeException("검색어가 포함된 유저가 존재하지 않습니다.");
         }
-        return userSearchResult;
+
+        searchUserSeqList.forEach(u -> searchUserList.add(profileService.selectFollowingUser(loggedInUser, userId, u)));
+
+        return searchUserList;
     }
 
     /**
