ebfabe3ccaaab5bd10275fd4e08579e4e1ad1928
C0012S <alwaysh1002@gmail.com>
2023-05-21 23:25:55 +0900

    [Update] ReplyDTO

--- poorband/src/main/java/com/finalproject/mvc/sobeit/dto/ReplyDTO.java
+++ poorband/src/main/java/com/finalproject/mvc/sobeit/dto/ReplyDTO.java
@@ -18,8 +18,12 @@ public class ReplyDTO {
     private String reply_text;
     private Long parent_reply_seq;
     private LocalDateTime written_date;
-    private int is_updated;
-    private String nickname;
-    private String profile_image_url;
-    private int reply_like_cnt;
+    private int is_updated; // 댓글 수정 여부
+    private String nickname; // 댓글을 작성한 사용자의 닉네임
+    private String user_tier; // 댓글을 작성한 사용자의 티어
+    private String profile_image_url; // 댓글을 작성한 사용자의 프로필 이미지 URL
+    private int reply_like_cnt; // 댓글 좋아요 개수
+    private boolean is_article_writer; // 댓글을 작성한 사용자와 글의 작성자가 같은지
+    private boolean is_reply_writer; // 댓글을 작성한 사용자가 자신인지
+    private boolean is_clicked_like; // 사용자가 해당 댓글에 좋아요를 클릭했는지
 }
